95958682421116702414aad50aa5e7fe7b296bf1
Omkar Rakshe <omkarrakshe2808@gmail.com>
2026-01-16 23:55:17 +0530

    Initial commit: Electron to-do list desktop app with modern UI

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 900,
+    height: 700,
+    minWidth: 600,
+    minHeight: 500,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    backgroundColor: '#0f0f1e',
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, 'build/icon.png')
+  });
+
+  mainWindow.loadFile('index.html');
+  
+  // Open DevTools in development (optional)
+  // mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
